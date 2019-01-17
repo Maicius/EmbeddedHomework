712bec5fef89b42df97542d761453ae95daaa0b4
maicius <maicius@outlook.com>
2019-01-17 21:28:53 +0800

    添加文档

--- SR04_FIlter.ino
+++ SR04_FIlter.ino
@@ -6,35 +6,40 @@
 #define DEFAULT_PINGS 5
 #define DEFAULT_COUNT 9
 SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
-long raw_val;
+float raw_val;
 
-long median_val;
+float median_val;
 int median = 0;
-long median_distance;
-long distance_list[MAX_K];
+float median_distance;
+float distance_list[MAX_K];
 
-long avg_val;
-long avg_distance;
-long avgDistanceList[MAX_K];
+float avg_val;
+float avg_distance;
+float avgDistanceList[MAX_K];
 
-long median_filter(int k = 11);
-long avg_filter(int delay_time = DEFAULT_DELAY, int pings = DEFAULT_PINGS);
-long median_avg_filter(int k = 11);
-
-long median_avg_val;
-long median_avg_distance;
-long avg_distance_list[MAX_K];
+float median_filter(float raw_val, int length_list, int k);
+float avg_filter(float raw_val, int length_list, int k);
+float median_avg_filter(float raw_val, int length_list, int k);
+float getDistance();
+float median_avg_val;
+float median_avg_distance;
+float avg_distance_list[MAX_K];
 void setup() {
   Serial.begin(9600);
   delay(10);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
 int loop_count = 0;
 int length_list;
 int k;
 void loop() {
-  raw_val = sr04.Distance();
-  
+  //官方库，获取的值为long int型
+//  raw_val = sr04.Distance();
+  //  自定义方法，获取的值为float型，更精确
+  raw_val = getDistance();
   if (loop_count > DEFAULT_COUNT) {
     k = loop_count % DEFAULT_COUNT;
     length_list = DEFAULT_COUNT;
@@ -50,17 +55,19 @@ void loop() {
   Serial.print(',');
   Serial.print(avg_distance);
   Serial.print(',');
-  Serial.println(median_avg_distance);
+  Serial.print(median_avg_distance);
+  Serial.print(',');
+  Serial.println(100);
   loop_count++;
   delay(100);
 }
 
-
-long median_filter(long raw_val,int length_list,int k) {
+// 中位值滤波
+float median_filter(float raw_val, int length_list, int k) {
 
   distance_list[k] = raw_val;
   // 插入排序
-  long distance_sort_list[DEFAULT_COUNT];
+  float distance_sort_list[DEFAULT_COUNT];
   for (int i = 0; i < length_list; i++) {
     median_val = distance_list[i];
     int j = i;
@@ -77,12 +84,12 @@ long median_filter(long raw_val,int length_list,int k) {
 }
 
 // 均值滤波
-long avg_filter(long raw_val,int length_list,int k) {
-  
+float avg_filter(float raw_val, int length_list, int k) {
+
   avgDistanceList[k] = raw_val;
-  long sum = 0;
-  for (int i=0; i< length_list; i++){
-      sum += avgDistanceList[i];
+  float sum = 0;
+  for (int i = 0; i < length_list; i++) {
+    sum += avgDistanceList[i];
   }
   avg_val = sum / length_list;
   format_output("均值滤波：", avg_val);
@@ -90,30 +97,47 @@ long avg_filter(long raw_val,int length_list,int k) {
 }
 
 // 中位值平均滤波
-long median_avg_filter(long raw_val,int length_list,int k) {
-
+float median_avg_filter(float raw_val, int length_list, int k) {
   avg_distance_list[k] = raw_val;
-  
+  float distance_sort_list_2[DEFAULT_COUNT];
   for (int i = 0; i < length_list; i++) {
     median_avg_val = avg_distance_list[i];
     int j = i;
-    while (median_avg_val < avg_distance_list[j - 1] && j > 0) {
-      avg_distance_list[j] = avg_distance_list[j - 1];
+    while (median_avg_val < distance_sort_list_2[j - 1] && j > 0) {
+      distance_sort_list_2[j] = distance_sort_list_2[j - 1];
       j--;
     }
-    avg_distance_list[j] = median_avg_val;
+    distance_sort_list_2[j] = median_avg_val;
   }
-  long sum = 0;
-  for (int i = 1; i < k - 1; i++) {
-    sum += avg_distance_list[i];
-  }
-  median_avg_val = int(sum / (k - 2));
 
+  if (length_list > 2) {
+    float sum = 0;
+    for (int i = 1; i < length_list - 1; i++) {
+      sum += distance_sort_list_2[i];
+    }
+    median_avg_val = float(sum / (length_list - 2));
+  }
+  else {
+    median_avg_val = avg_distance_list[k];
+  }
+  format_output("中位值均值滤波：", median_avg_val);
   return median_avg_val;
 }
 
-void format_output(const char* name, long val) {
+void format_output(const char* name, float val) {
   Serial.print(name);
   Serial.print(val);
   Serial.println("cm");
+}
+
+float getDistance() {
+  float cm;
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  cm = pulseIn(ECHO_PIN, HIGH) / 58.82; //算成厘米
+  cm = (int(cm * 100.0)) / 100.0; //保留两位小数
+  return cm;
 }
